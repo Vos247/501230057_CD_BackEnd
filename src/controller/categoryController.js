d005ce880548f2665e97b34cacc4d76c624ec6b9
Vos247 <nttvinh2004@gmail.com>
2025-02-13 21:53:00 +0700

    pagination and page size

--- src/controller/categoryController.js
+++ src/controller/categoryController.js
@@ -1,19 +1,27 @@
 import CategoryModel from "../models/categoryModel.js";
 import { ObjectId } from "mongodb";
-import { removeVietnameseAccents } from "../common/index.js";
+import { removeVietnameseAccents } from "../common/index.js"; // loại bỏ dấu tiếng việt để dễ tìm kiếm
 export async function listCategory(req, res){
     const search = req.query?.search
+    const pageSize = !!req.query.pageSize ? parseInt(req.query.pageSize) : 5
+    const page = !!req.query.page ? parseInt(req.query.page) : 1
+    const skip = (page-1) * pageSize
     const filters = {
-        deleteAt: null
+        deleteAt: null // chưa bị xóa 
     }
     if(search && search.length > 0){
-        filters.searchString = {$regex: removeVietnameseAccents(search), $options: "i"}
+        filters.searchString = {$regex: removeVietnameseAccents(search), $options: "i" } // $option không phân biệt chữ hoa thường 
         };
     try {
-    const categories = await CategoryModel.find(filters);
+    const countCategories = await CategoryModel.countDocuments(filters); 
+    const categories = await CategoryModel.find(filters).skip(skip).limit(pageSize); 
+    // res.json(categories)
     res.render("pages/categories/list", {
         title: "categories",
         categories: categories,
+        countPagination: Math.ceil(countCategories/pageSize),
+        pageSize,
+        page
     })
     } catch(e){
     res.send("Lỗi lấy danh sách category");
@@ -23,16 +31,16 @@ export async function renderpageCreateCategory(req, res){
     res.render("pages/categories/form", {
         title: "Create categories",
         mode: "Create",
-        category: {}
+        category: {} // mặc định là rổng vì đang tạo mới
     });
 }
 export async function createCategory(req, res){
-    const data = req.body;
+    const data = req.body; // lấy dữ liệu form đã nhập thông qua post
     try {
     await CategoryModel.create({
-       ...data, createAt: new Date()
+       ...data, createAt: new Date() // ...data sao chép toàn bộ dữ liệu người dùng đã nhập , new date để lấy thời gian hiện tại
     });
-    res.redirect("/categories")
+    res.redirect("/categories") // nếu tạo thành công sẽ chuyển đến trang danh sách 
     }catch(e){
     res.send("Tạo sản phẩm không thành công");
     }
@@ -40,13 +48,13 @@ export async function createCategory(req, res){
 
 export async function renderpageUpdateCategory(req, res){
     try {
-        const {id} = req.params;
-    const category = await CategoryModel.findOne({_id: new ObjectId(id), deleteAt: null});
+        const {id} = req.params; // lấy danh mục cần sửa
+    const category = await CategoryModel.findOne({_id: new ObjectId(id), deleteAt: null}); // lấy id cần tìm và đảm bảo chưa bị xóa
     if(category) {
         res.render("pages/categories/form", {
         title: "update categories",
         mode: "Update",
-        category: category
+        category: category // chứa danh mục form
     });
     }else {
         res.send("Không thấy sản phẩm nào phù hợp");
@@ -56,17 +64,17 @@ export async function renderpageUpdateCategory(req, res){
     }
 }
 export async function updateCategory(req, res){
-    const {id, ...data} = req.body;
+    const {id, ...data} = req.body; // khai báo 2 biến dùng để chứa dữ liệu từ form và ID với data cần cập nhật
     try {
-    await CategoryModel.updateOne(
+    await CategoryModel.updateOne( 
         {_id: new ObjectId(id)},
         {
-            ...data,
+            ...data,    // điều kiện xác nhận tài liệu cần cập nhật và cập nhật trong các trường hợp của data
         updateAt: new Date()
     });
-    res.redirect("/categories")
+    res.redirect("/categories") 
     }catch(e){
-    res.send("Cập nhật sản phẩm không thành công");
+    res.send("Cập nhật sản phẩm không thành công"); 
     }
 }
 
